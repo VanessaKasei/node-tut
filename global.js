b9f0d02fa0eee0aff61839d33b7d446893b6a097
VanessaKasei <kaseivanessa@gmail.com>
2024-09-02 13:23:10 +0300

    first commit

---
+++ global.js
@@ -0,0 +1,6 @@
+console.log(__dirname);
+
+setInterval( () => {
+    console.log("You are doing great Vanessa,keep it up!");
+    
+},1000);
---
+++ http.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+
+  res.write("welcome to our home page");
+  res.end();
+}); 
+
+server.listen(4000);
